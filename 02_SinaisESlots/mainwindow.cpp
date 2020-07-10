b41cf765badb63cf2514ca52cddd4ddeb41e4b27
Rodrigo <rodrigoricha@gmail.com>
2020-07-10 17:18:38 -0300

    Alterações com as possíveis formas de utilização de signal e slots para uma mesma view

--- 02_SinaisESlots/mainwindow.cpp
+++ 02_SinaisESlots/mainwindow.cpp
@@ -6,10 +6,23 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->pushButton_3, SIGNAL(clicked()), this, SLOT(showNormal()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString css = "background-color:#f00;color:#000";
+    ui->label->setStyleSheet(css);
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    this->setStyleSheet("background-color:#f00");
 }
 
