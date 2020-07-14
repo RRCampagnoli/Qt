f0160d16aebfb31579a0ef4a964f1ba9038ad70b
Rodrigo <rodrigoricha@gmail.com>
2020-07-14 18:23:15 -0300

    Inserindo botão para troca de imagem
    Uma demonstração simples de como alterar a imagem em um clique

--- 07_ImagemEmForms/mainwindow.cpp
+++ 07_ImagemEmForms/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QPixmap img_endGame("/home/rodrigo/Imagens/20190424avengers-endgame-001a.jpg");
+    ui->lb_endGame->setPixmap(img_endGame.scaled(405,393, Qt::KeepAspectRatio));
+    //ui->lb_endGame->setPixmap(img_endGame.scaled(810,786, Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +18,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QPixmap img_posterBrasileiroUltimato("/home/rodrigo/Imagens/poster-brasileiro-de-vingadores-ultimato-1556045801213_v2_1225x1800.jpg");
+    ui->lb_endGame->setPixmap(img_posterBrasileiroUltimato.scaled(405,393, Qt::KeepAspectRatio));
+}
