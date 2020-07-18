002550bedeb389115ca1dbcb034a648bf219fd3d
Rodrigo <rodrigoricha@gmail.com>
2020-07-17 21:17:09 -0300

    Finalizado
    Apenas imputando a condição de password para o campo senha e outros pequenos detalhes

--- 10_TelaLogin/mainwindow.cpp
+++ 10_TelaLogin/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tx_usuario->setText("Samuel");
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_bt_login_clicked()
+{
+    QString usr = ui->tx_usuario->text();
+    QString pwd = ui->tx_senha->text();
+    qDebug() << "Usuário: " + usr + " "<< "Senha: " + pwd;
+
+    if(usr.compare("adm") ==0 && pwd.compare("adm") == 0)
+        QMessageBox::information(this, "Login", "Sucesso!");
+    else
+        QMessageBox::critical(this, "Login", "Login e/ou senha errado(s)!");
+
+}
+
+void MainWindow::on_bt_cancel_clicked()
+{
+    ui->tx_usuario->clear();
+    ui->tx_senha->clear();
+}
--- 10_TelaLogin/mainwindow.h
+++ 10_TelaLogin/mainwindow.h
@@ -15,6 +15,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_bt_login_clicked();
+
+    void on_bt_cancel_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
